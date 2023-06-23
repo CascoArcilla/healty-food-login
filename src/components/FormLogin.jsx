c8357460c5ebde4149fbab9f41ac99bd70edc6e4
CascoArcilla <normon1230@gmail.com>
2023-06-23 14:14:21 -0600

    Decoracion Descripcion

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -3,12 +3,14 @@ import BotonLogin from "./BotonLogin";
 import EncabezadoLogin from "./EncabezadoLogin";
 import InputCorreoLogin from "./InputCorreoLogin";
 import InputPassLogin from "./InputPassLogin";
+import NormalLogin from "./NormalLogin";
 
 export default function FormLogin({ submit }) {
   return (
     <>
       <EncabezadoLogin />
       <AlterLogin />
+      <NormalLogin />
       <form onSubmit={submit}>
         <div className="content-form">
           <InputCorreoLogin />
--- src/components/InputCorreoLogin.jsx
+++ src/components/InputCorreoLogin.jsx
@@ -1,34 +1,19 @@
 export default function InputCorreoLogin() {
   return (
     <div className="input-correo-login">
-      <label for="input-group-1" class="block mb-2 text-lg font-medium text-gray-900">Correo</label>
+      <label for="input-group-1" class="block mb-2 text-xl font-medium text-gray-900">Correo</label>
       <div class="relative mb-6">
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
         </div>
-        <input type="text" id="input-group-1" class="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 placeholder-gray-300" placeholder="name@algo.com"></input>
+        <input
+          type="text"
+          id="input-group-1"
+          placeholder="Su e-mail"
+          class="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 placeholder-gray-300"
+        >
+        </input>
       </div>
     </div>
   )
 }
-
-/*
-<div _ngcontent-gry-c92="" class="mb-3"><div _ngcontent-gry-c92="" class="viva-input-field"><input _ngcontent-gry-c92="" apppwdtoggle="" type="password" id="login_password" name="login_password" formcontrolname="password" maxlength="32" class="viva-personal ng-pristine ng-invalid ng-touched" placeholder="Contraseña"><i _ngcontent-gry-c92="" class="fas fa-key"></i><label _ngcontent-gry-c92="" for="login_password"> Contraseña </label><div _ngcontent-gry-c92="" class="divider"></div><i class="fas fa-eye pwd-toggle"></i></div><small _ngcontent-gry-c92="" class="viva-input-field-error ng-star-inserted"> Este campo es obligatorio.<br _ngcontent-gry-c92=""></small><!----></div>
-*/
-
-/*
-<label for="input-group-1" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
-<div class="relative mb-6">
-  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
-  </div>
-  <input type="text" id="input-group-1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com">
-</div>
-<label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-<div class="flex">
-  <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-    @
-  </span>
-  <input type="text" id="website-admin" class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="elonmusk">
-</div>
-*/
